bbec637b0b4321cd59c3524024079d2a43812095
Alisa Duncan <alisa.duncan@engagedtechnologies.com>
2017-10-08 09:54:43 -0500

    first commit

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,70 @@
+import { ProductEditInfoComponent } from './product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit-tags.component';
+import { ProductResolver } from './product-resolver.service';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit.component';
+
+import { ProductFilterPipe } from './product-filter.pipe';
+import { ProductService } from './product.service';
+
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'products',
+        children: [
+          {
+            path: '',
+            component: ProductListComponent
+          },
+          {
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { product: ProductResolver }
+          },
+          {
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { product: ProductResolver },
+            children: [
+              {
+                path: '',
+                redirectTo: 'info',
+                pathMatch: 'full'
+              },
+              {
+                path: 'info',
+                component: ProductEditInfoComponent
+              },
+              {
+                path: 'tags',
+                component: ProductEditTagsComponent
+              }
+            ]
+          }
+        ]
+      },
+
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    ProductFilterPipe,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
+  ],
+  providers: [
+    ProductService,
+    ProductResolver
+  ]
+})
+export class ProductModule { }
